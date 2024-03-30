8e7b9e706393d2f2f8c9deb73f7262c663d5f948
Christopher Bishop <cbishop@cibgraphics.com>
2024-03-30 15:29:39 -0600

    prep for new hero

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,10 +1,3 @@
 $(function() {
-    
-  var scene = document.getElementById('scene');
-  var parallaxInstance = new Parallax(scene, {
-    selector : '.layer',
-    hoverOnly: true,
-    limitY: 0
-  });
   
 });
